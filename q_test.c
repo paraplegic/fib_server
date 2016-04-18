75354c6ee85fd6f3a282fcb66bbe5a6fadf5245e
Rob Sciuk <rob@controlq.com>
2016-04-18 15:35:00 -0400

    test now is consistent at 10M iterations

--- q_test.c
+++ q_test.c
@@ -6,6 +6,7 @@
 #define rqst_EXIT	-1
 #define FOREVER		(1 == 1)
 #define Q_SIZE		512
+#define N_THREADS	4
 
 // must have usage info ...
 void usage( int argc, char **argv )
@@ -21,7 +22,7 @@ int bucket[4] = {0};
 
 int main( int argc, char **argv )
 {
-   int wk_socket, cx, max ;
+   int wk_socket, nx, cx, max ;
    pthread_t *threads[10];
 
    if( argc < 2 )
@@ -34,32 +35,28 @@ int main( int argc, char **argv )
    // input and work queues created here ...
    inp_Q = q_create( Q_SIZE, (Func_t *) task_del ) ;
 
-   cx = 0 ; 
-   // just a few worker threads: 4 readers ... 
-   pthread_create( (pthread_t *) &threads[cx++], NULL, cx_read_task, (void *) 0 ) ;
-   pthread_create( (pthread_t *) &threads[cx++], NULL, cx_read_task, (void *) 1 ) ;
-   pthread_create( (pthread_t *) &threads[cx++], NULL, cx_read_task, (void *) 2 ) ;
-   pthread_create( (pthread_t *) &threads[cx++], NULL, cx_read_task, (void *) 3 ) ;
-
-//   Task_t *T = task_crt( 0, rqst_CONTINUE, -1 ) ;
-   for( cx = 0 ; cx < max ; cx++ )
+   // just a few worker threads: N_THREADS readers ... 
+   for( cx = 0 ; cx < N_THREADS ; cx++ )
    {
-      q_push( inp_Q, task_crt( 0, rqst_CONTINUE, -1 ) ) ;
-//      q_push( inp_Q, T ) ;
+     nx = pthread_create( (pthread_t *) &threads[cx], NULL, cx_read_task, (void *) ((long) cx) ) ;
+     if( nx != 0 )
+     {
+        printf( "create thread %d returns %d.\n", cx, nx ) ; 
+        exit( 1 ) ; 
+     }
    }
 
-/*
-   T->request = rqst_EXIT ;
-   q_push( inp_Q, T ) ;
-   q_push( inp_Q, T ) ;
-   q_push( inp_Q, T ) ;
-   q_push( inp_Q, T ) ;
-*/
+   for( cx = 0 ; cx < max ; cx++ )
+   {
+      // the task_crt does a malloc of a structure ...
+      q_push( inp_Q, task_crt( 0, rqst_CONTINUE, -1 ) ) ;
+   }
+
    q_push( inp_Q, task_crt( 0, rqst_EXIT, -1 ) ) ;
    q_push( inp_Q, task_crt( 0, rqst_EXIT, -1 ) ) ;
    q_push( inp_Q, task_crt( 0, rqst_EXIT, -1 ) ) ;
    q_push( inp_Q, task_crt( 0, rqst_EXIT, -1 ) ) ;
-   sleep( 1 ) ;
+   sleep( 2 ) ;
 
    int sum = 0 ; 
    for( cx = 0 ; cx < 4 ; cx++ )
@@ -86,15 +83,20 @@ void *cx_read_task( void *arg )
   {
     T = q_pop( inp_Q ) ;
     if( isNul( T ) )
+    {
+      printf( "Null task detected in thread %d.\n", thread ) ;
       continue ;
+    }
 
     if( T->request == rqst_EXIT )
     {
       printf( "Thread %d terminated.\n", thread ) ;
       pthread_exit( (void *) NULL ) ; 
     }
-    T = task_del( T ) ;
     bucket[thread] += 1;
+
+    // free's the structure 
+    T = task_del( T ) ;
   }
   return (void *) NULL ;
 }
